6b53b1a2d5d0d40928265aa976f9acf814c2d0e3
lobov1 <60486793+lobov1@users.noreply.github.com>
2020-04-07 19:55:31 -0400

    updating to connect to db

--- insert.php
+++ insert.php
@@ -15,8 +15,8 @@ if (!empty($username) || !empty($password) || !empty($gender) || !empty($email) 
     if (mysqli_connect_error()) {
      die('Connect Error('. mysqli_connect_errno().')'. mysqli_connect_error());
     } else {
-     $SELECT = "SELECT email From TestTable Where email = ? Limit 1";
-     $INSERT = "INSERT Into TestTable (username, password, gender, email, phoneCode, phone) values(?, ?, ?, ?, ?, ?)";
+     $SELECT = "SELECT email From TestHtml Where email = ? Limit 1";
+     $INSERT = "INSERT Into TestHtml (username, password, gender, email, phoneCode, phone) values(?, ?, ?, ?, ?, ?)";
      //Prepare statement
      $stmt = $conn->prepare($SELECT);
      $stmt->bind_param("s", $email);
